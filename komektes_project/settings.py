6d4d1a52535300879768519afe83410077d62635
SSBCOD <sakenserikov58@gmail.com>
2026-06-18 17:01:46 +0500

    fix: add CSRF_TRUSTED_ORIGINS support

--- komektes_project/settings.py
+++ komektes_project/settings.py
@@ -29,6 +29,10 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS', 'http://localhost'
+).split(',')
+
 
 # Application definition
 
